b227d75f7f7e7c739e404027041414328f08bd05
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-09-29 12:00:37 +0200

    Exercice1 update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$a = 2;
-$b = 6;
+  $var1 = 5;
+  $var2 = 9;
 
-echo $a + $b
-
+  echo ("Le resultat de l'addition est: " .($var1 + $var2) .'<br>');
+  echo "Le resultat du la multiplication est: " .($var1 * $var2);
  ?>
